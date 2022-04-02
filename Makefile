b581d8828988c89d448087c448eb972692c88a6f
slowv <zeev1990@gmail.com>
2022-04-02 23:41:42 +0300

    aaa

---
+++ Makefile
@@ -0,0 +1,5 @@
+lists_ab: lists_ab.c
+	gcc -g -Wall -o lists_ab lists_ab.c
+
+  clean:
+	  $(RM) lists_ab
--- lists_ab.c
+++ lists_ab.c
@@ -9,15 +9,19 @@ int countAlphaNumeric(char *str_arr );
 void printString(char *str_arr);
 void printMetadata(char *str_arr, int alphaNumeric, int length );
 
-void main()
+int main()
 {
-    //define
+    /**
+     * this function get a char array from user in a dynamic way (can be any size)
+     * and counts how many alphanumeric characters in array
+     */
+    /*define*/
     int current_size = 0;
     int alphanum_count = 0;
     char *str_arr;
     str_arr = malloc(sizeof( char ));
 
-    //do
+    /*do*/
     current_size = getArray(str_arr);
     alphanum_count = countAlphaNumeric(str_arr);
 
@@ -25,14 +29,20 @@ void main()
     printMetadata(str_arr, alphanum_count, current_size );
 
 
-    //cleanup
+    /*cleanup*/
     free(str_arr);
     str_arr = NULL;
 
+    return 0;
 }
 
 int getArray(char *str_arr)
 {
+    /**
+     * function that get a char array from user, dynamic memory
+     * @param str_arr - char pointer that will store our array
+     * @return - int, array size after input
+     */
     int end = EOF;
     int i = 0;
     int current_size = 1;
@@ -42,12 +52,12 @@ int getArray(char *str_arr)
     if(str_arr != NULL)
     {
 
-        //accept user input until hit enter or end of file
+        /*accept user input until hit enter or end of file*/
         while ((end = getchar()) != '\n' && end != EOF)
         {
             str_arr[i++] = (char) end;
 
-            //if i reached maximize size then realloc size
+            /*if i reached maximize size then realloc size*/
             if (i == current_size)
             {
                 current_size = i + 1;
@@ -62,6 +72,10 @@ int getArray(char *str_arr)
 
 void printString(char *str_arr)
 {
+    /**
+     * function that prints string nicely.
+     * @param str_arr - char pointer, our array
+     */
     int i = 0;
     printf("\nLong String value: \n");
     while (str_arr[i] != '\0')
@@ -76,6 +90,11 @@ void printString(char *str_arr)
 
 int isAlphaNumeric(char ch)
 {
+    /**
+     * return 1 if char is alphanumeric , 0 if not
+     * @param ch - char
+     * @return - int, 1 = True, 0 = False
+     */
     if ((ch >= '0' & ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
         return 1;
 
@@ -83,6 +102,11 @@ int isAlphaNumeric(char ch)
 }
 int countAlphaNumeric(char *str_arr )
 {
+    /**
+     * function that counts how many alphaNumeric chars there are in our char array
+     * @param str_arr - char pointer, our array
+     * @return - int,  how many alphanumeric characters in array
+     */
     int counter = 0;
     int i =0;
     while (str_arr[i] != '\0')
@@ -95,6 +119,13 @@ int countAlphaNumeric(char *str_arr )
 
 void printMetadata(char *str_arr, int alphaNumeric, int length )
 {
+    /**
+     * function prints some metadata given
+     * @param str_arr - char pointer, our array
+     * @param alphaNumeric - int, how many alphanumeric characters in array
+     * @param length- int, length of the array
+     *
+     */
     printString(str_arr);
     printf("Total number of characters:\n%d\n",length );
     printf("Number of alphaNumeric characters:\n%d\n",alphaNumeric );
